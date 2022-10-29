29ff9764f39038c5f4acb3dcf92a01d5c6a48c0d
yashpuneet <yash.puneet02@gmail.com>
2022-10-29 02:36:37 -0700

    Added volume controls with icon change

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -6,6 +6,7 @@ function init()
 {
   const hornFrame = document.querySelector("img[alt='No image selected']");
   const hornSound = document.querySelector("audio.hidden");
+  const volIcon = document.querySelector("img[alt='Volume level 2']");
   const jsConfetti = new JSConfetti();
 
   const dropdown = document.getElementById("horn-select");
@@ -25,5 +26,32 @@ function init()
     }
    
     hornSound.play();
+  });
+
+  const slider = document.querySelector("input[name='volume']");
+  slider.addEventListener('input', function()
+  {
+    //console.log(this.value);
+    let vol = this.value;
+    if(vol == 0)
+    {
+      volIcon.src = "/assets/icons/volume-level-0.svg";
+      hornSound.volume = vol/100;
+    }
+    else if(vol > 0 && vol < 33)
+    {
+      volIcon.src = "/assets/icons/volume-level-1.svg";
+      hornSound.volume = vol/100;
+    }
+    else if(vol > 32 && vol < 67)
+    {
+      volIcon.src = "/assets/icons/volume-level-2.svg";
+      hornSound.volume = vol/100;
+    }
+    else
+    {
+      volIcon.src = "/assets/icons/volume-level-3.svg";
+      hornSound.volume = vol/100;
+    }
   });
 }
