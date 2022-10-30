0815b467587c205e2ec1bfdc8bb8159bf53a9a44
yashpuneet <yash.puneet02@gmail.com>
2022-10-30 00:24:55 -0700

    Populated dropdown with voice options

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,6 +3,23 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
+  const voicesDropdown = document.getElementById("voice-select");
+  
+  const speaker = window.speechSynthesis;
+  let voices = [];
+  setTimeout(() => {
+    voices = speaker.getVoices();
+    for(let i = 0; i < voices.length; i++)
+    {
+      //console.log(voices[i].name);
+      const option = document.createElement("option");
+      option.textContent = voices[i].name;
+      option.value = i;
+      voicesDropdown.appendChild(option);
+    }
+  }, 100);
+
+
   const textCapture = document.getElementById("text-to-speak");
 
   const speak = document.querySelector("button");
